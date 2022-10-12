63abc6fcc489c4ee15d3ec25528eb1056fb50fbf
XSZ <1273343014@qq.com>
2022-10-12 23:34:26 +0800

    修改pom.xml文件

---
+++ src/main/java/com/mengying/blogxu/BlogXuApplication.java
@@ -0,0 +1,13 @@
+package com.mengying.blogxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogXuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogXuApplication.class, args);
+    }
+
+}
